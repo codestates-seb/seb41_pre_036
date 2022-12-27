43faebd587ed7c78c29a1e94309153fcbf9e52db
Hyun Seok Park <luckseok1@gmail.com>
2022-12-27 12:23:06 +0900

    [FE]feat: Sidebar 클릭시 해당 라우터 설정 및 CSS 설정

--- client/src/pages/Tags.js
+++ client/src/pages/Tags.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LayoutContainer from "../components/LayoutContainer";
+import PageContainer from "../components/PageContainer";
+import Sidebar from "../components/Sidebar";
 
 export default function Tags() {
-  return <div>Tags</div>;
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <LayoutContainer>
+        <PageContainer>tag</PageContainer>
+      </LayoutContainer>
+      <Footer />
+    </>
+  );
 }
