89e505b3dee5e01e927b46b2e21a927e37aac7d8
Hyun Seok Park <luckseok1@gmail.com>
2022-12-27 17:14:10 +0900

    [FE]feat:Tag 페이지 기능 구현

--- client/src/components/PageContainer.js
+++ client/src/components/PageContainer.js
@@ -8,10 +8,10 @@ export const Container = styled.div`
   @media screen and (min-width: 641px) {
     margin-left: 164px;
   }
-  @media screen and (min-width: 981px) {
+  /* @media screen and (min-width: 981px) {
     display: flex;
     justify-content: space-between;
-  }
+  } */
   > main {
     width: 100%;
     @media screen and (min-width: 981px) {
